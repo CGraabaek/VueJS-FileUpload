d5d241711ed1529a94ced22d1d87e7ee4d58c481
Christian Graabæk Steenfeldt <c.graabaek@gmail.com>
2020-04-23 09:56:49 +0200

    Added the initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# VueJS-FileUpload
-A simple project showing drag and drop file-upload in VueJS
+# file-upload
+
+> File upload template
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/FileUpload.vue
@@ -0,0 +1,91 @@
+<template>
+  <div
+    v-cloak
+    @drop.prevent="addFile"
+    @dragover.prevent
+    class="file-upload"
+    id="file-upload"
+    role="dialog"
+    aria-describedby="pico-1"
+    style="display: block; position: fixed; z-index: 10001; left: 50%; top: 38.1966%; max-height: 90%; box-sizing: border-box; width: auto; transform: translate(-50%, -38.1966%); overflow: auto; background-color: white; padding: 20px; border-radius: 5px;"
+  >
+    <h1>FileUpload</h1>
+    <h4>Drag and drop one or more files here</h4>
+    <ul>
+      <li v-for="file in this.files">
+        {{ file.name }} ({{ file.size | kb }} kb)
+        <button @click="removeFile(file)" title="Remove">X</button>
+      </li>
+    </ul>
+
+    <button v-if="!this.uploaded" :disabled="uploadDisabled" @click="upload">Upload</button>
+    <p>{{status}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FileUpload",
+  data() {
+    return {
+      files: [],
+      uploaded: false,
+      status: ""
+    };
+  },
+  computed: {
+    uploadDisabled() {
+      return this.files.length === 0;
+    }
+  },
+  methods: {
+    addFile(e) {
+      let droppedFiles = Array.from(e.dataTransfer.files);
+      if (!droppedFiles) return;
+      // this tip, convert FileList to array, credit: https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+      [...droppedFiles].forEach(f => {
+        this.files.push(f);
+      });
+    },
+    removeFile(file) {
+      this.files = this.files.filter(f => {
+        return f != file;
+      });
+    },
+    upload() {
+      let formData = new FormData();
+      this.files.forEach((f, x) => {
+        formData.append("file" + (x + 1), f);
+      });
+
+      fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: formData
+      })
+        .then(res => res.json())
+        .then(res => {
+          this.uploaded = true;
+          this.status = "File uploaded successfully";
+          console.log("done uploading", res);
+        })
+        .catch(e => {
+          console.error(JSON.stringify(e.message));
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+a {
+  color: #42b983;
+}
+
+.file-upload {
+  background-color: #42b983 !important;
+}
+</style>
